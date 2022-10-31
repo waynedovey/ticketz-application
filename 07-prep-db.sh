4a80d6abb10e352eb836a81c6f8f6c3eb64cdf56
Wayne Dovey <wdovey@gmail.com>
2022-11-01 07:33:19 +1100

    all

--- 07-prep-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-oc project ticketz
-
-POD=$( oc get pods -l deployment=seats-db -o name | sed 's^pod/^^g')
-
-oc cp db/setupdb.sh $POD:/tmp/setupdb.sh
-oc rsh $POD /tmp/setupdb.sh
--- db/setupdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo 'use seats-db
-db.seats.insertOne( {"seatId": NumberInt(1), "customerId": NumberInt(0), "state": "available", "category": "A", "timestamp": "2022"})
-db.seats.createIndex( { "seatId": 1 } )
-db.seats.createIndex( { "state": 1 } )' | \
-    mongo -u admin -p admin --authenticationDatabase admin
-
